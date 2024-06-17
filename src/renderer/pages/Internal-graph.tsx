3eb167a9e30a6ea4d591831a2941b2b3c9da1397
Yash Shukla <68775936+y2kshukla@users.noreply.github.com>
2024-06-17 18:48:04 +0530

    setting

---
+++ src/renderer/pages/Internal-graph.tsx
@@ -0,0 +1,13 @@
+import { Button, Typography } from '@mui/material';
+
+function InternalGraph() {
+  return (
+    <div>
+      <h1>electron-react-boilerplate</h1>
+      <Button variant="outlined">Click me</Button>
+      <Typography>Hellow</Typography>
+    </div>
+  );
+}
+
+export default InternalGraph;
